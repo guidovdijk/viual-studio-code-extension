bae9f00ce9d9948564639a03d9f658083713701d
Guido van Dijk <GuidoD@Guido-van-Dijk.local>
2019-10-12 13:50:39 +0200

    Return null if the prefix is not on the same line as the clickedTag

--- src/providers/selector-definition-provider.ts
+++ src/providers/selector-definition-provider.ts
@@ -3,52 +3,65 @@ import { TextDocument, Uri, Position, Location, ProviderResult } from 'vscode';
 
 export class SelectorDefinitionProvider implements vscode.DefinitionProvider {
 
+  /*
+   * TODO:
+   * Check if clickedTag has the prefix before it '{{>clickedTag', 
+   * now if '{{>clickedTag' and 'clickedTag' without the prefix are on the same line
+   * both words redirect to the file
+   * 
+  */
   provideDefinition(document: TextDocument, position: Position): ProviderResult<Location> {
     const wordRange = document.getWordRangeAtPosition(position);
-
     const clickedTag = document.getText(wordRange);
+    const activeEditor = vscode.window.activeTextEditor;
 
-    const findTagInDocumentRegex = new RegExp(`{{>${clickedTag}`, 'i');
-    // const findInputAttributeInDocumentRegex = new RegExp(`@Input\\(['"]?\\\w*['"]?\\)\\s+${clickedTag}`)
-    console.log({position, wordRange,
-    clickedTag}, );
-    return vscode.workspace.findFiles('{**/*.html}', 'node_modules/*')
+    const config = {
+      source: '{**/*.html}',
+      prefix: "{{>",
+    };
+
+    if (activeEditor) {
+      const selection = activeEditor.selection;
+      const text = activeEditor.document.lineAt(selection.start.line).text;
+      if(text.includes(config.prefix) === false){
+        return null;
+      }
+    }
+
+    return vscode.workspace.findFiles(config.source, 'node_modules/*')
       .then(tsFiles => {
         return tsFiles.map(file => {
           return vscode.workspace.openTextDocument(Uri.file(file.fsPath))
             .then(document => {
               const tagMatch = file.fsPath.includes(clickedTag);
-              // const attributeMatch = findInputAttributeInDocumentRegex.test(document.getText())
-              let lineNumber = 0
-              let colNumber = 0
+              const lineNumber = 0;
+              const colNumber = 0;
               
-
               return {
                 path: file.fsPath,
-                match: tagMatch,// || attributeMatch,
+                match: tagMatch,
                 lineNumber,
                 colNumber,
-              }
-            })
-        })
+              };
+            });
+        });
       })
       .then(mappedTsFiles => {
         return Promise.all(mappedTsFiles)
           .then(tsFileObjects => {
             const matchedTsFileObject = tsFileObjects.find((mo => {
                 return mo.match;
-            }))
-            return matchedTsFileObject ? matchedTsFileObject : null
-          })
+            }));
+            return matchedTsFileObject ? matchedTsFileObject : null;
+          });
       })
       .then(tagDefinitionPath => {
         if (tagDefinitionPath === null) {
           // Returning null prevents the tag from being underlined, which makes sense as there's no tag definition match.
-          return null
+          return null;
         }
-
         // Returning a location gives VS Code a hint where to jump to when Ctrl/Cmd + click is invoked on the tag.
-        return new Location(Uri.file(tagDefinitionPath.path), new Position(tagDefinitionPath.lineNumber, tagDefinitionPath.colNumber))
-      })
+        return new Location(Uri.file(tagDefinitionPath.path), new Position(tagDefinitionPath.lineNumber, tagDefinitionPath.colNumber));
+      });
   }
 }
